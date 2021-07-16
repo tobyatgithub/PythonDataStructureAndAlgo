b64095597ae1c988e1be4073859c8683f4e48187
tobyatgithub <toby.fangyuan@gmail.com>
2021-07-15 17:41:11 -0700

    combined previous folders into one big source of truth. imports working.

---
+++ data_structures/main.py
@@ -0,0 +1,9 @@
+"""
+Here we provide a place to play with exisitng code.
+"""
+import sys
+sys.path.append('/Users/toby/Documents/PythonWorks/PythonDataStructureAndAlgo/data_structures')
+
+from node import Node
+node = Node(1)
+print(node)
