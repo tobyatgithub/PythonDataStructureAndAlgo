4fb25ef6ff8c8f0ab374ab0cd999d05747dd78f9
tobyatgithub <toby.fangyuan@gmail.com>
2021-10-17 06:32:40 -0700

    better support for latex print out.

--- algorithms/countingSort.py
+++ algorithms/countingSort.py
@@ -19,32 +19,42 @@ def countingSort(A, B, k, demo=False):
     # a temporary working storage
     C = [0 for _ in range(k + 1)]
     if demo:
-        print(f"Step 1: intput = {A}. init an empty C array for 0,...,k: {C}")
+        print(f"Step 1: intput = {A}. init an empty C array for 0,...,k: {C}\n")
 
     # counting
     for num in A:
         C[num] += 1
     if demo:
-        print(f"Step 2: counting the frequency of each occurance C = {C}")
+        print(f"Step 2: counting the frequency of each occurance C = {C}\n")
 
     for i in range(1, k + 1):  # to include i = k
         C[i] += C[i - 1]
     if demo:
         print(
-            f"Step 3: make C[i] means the number of elements less than or equal to i = {C} "
+            f"Step 3: make C[i] means the number of elements less than or equal to i = {C} \n"
         )
 
     if demo:
-        print("Step 4: now we start reordering based on info from array C.")
+        print("Step 4: now we start reordering based on info from array C.\n")
     for j in range(len(A) - 1, -1, -1):
         B[C[A[j]] - 1] = A[j]
+        if demo:
+            # latex homework print
+            print(
+                f"j = {j}, we go find $A[j] = A[{j}] = {A[j]}$. "
+                f"And we look up $C[A[j]] = C[{A[j]}] = {C[A[j]]}$. "
+                f"This means that there are {C[A[j]]} values less or equal to {A[j]} in the input array. "
+                f"We thus put the object $A[j] = A[{j}]] = {A[j]}$ to the correct output position $B[C[A[j]]] = B[{C[A[j]]-1}]$. "
+            )
+            ## human-read print
+            # print(f"dsadas" f"{A}" f"{C}")
+            # print(f"We swap that A[j] = {A[j]} into B, and change B to: {B}")
+
         C[A[j]] -= 1
         if demo:
             print(
-                f"j = {j}, A[j] = {A[j]}, C[A[j]] = {C[A[j]] + 1}. "
-                + f"Subtract C[A[j]] by 1 and update C->{C}"
+                f"We update the output array B to ${B}$. And we subtract $C[A[j]]$ by 1 and update C to ${C}$.\n"
             )
-            print(f"We swap that A[j] = {A[j]} into B, and change B to: {B}")
 
     if demo:
         print(f"Finished: A = {A}, B = {B}, C={C}.")
